ad50be1eee869e71e54c757eb539eb8cc5145056
rolled-potatoes <keep4404@gmail.com>
2021-05-20 21:45:40 +0900

    tete

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,59 +1,185 @@
-const pcConfig = {
-  iceServers: [
-    {
-      urls: "stun:stun.l.google.com:19302",
-    },
-  ],
-};
-
-const log = (value) => console.log(value);
-const $ = (target, base = document) => base.querySelector(target);
-const emitSignerEvent = (data) => socket.emit("message", data);
-
-const room_name = "foo";
-const join_btn = $("#join");
-const local_video = $("#local-video");
-const remote_video = $("#remote-video");
-
+/**
+ * TODO
+ * [x] 소켓연결
+ * 소켓 커넥트, user 구독
+ * 해시 태그 보내는 이벤트
+ * 방구독
+ * RTC 통신
+ * disconnect 시에 flag 초기화
+ */
 let socket;
+let stompClient;
+let myName;
 let isOffer = false;
 let isReady = false;
+let localStream;
 let isStarted = false;
 let pc;
-let local_track;
-let remote_track;
+let roomId = "";
 
-join_btn.addEventListener("click", () => {
-  socket.emit("join");
-  emitSignerEvent("start");
-  if (isOffer) {
-    startConnection();
-  }
-});
+const $ = (target) => document.querySelector(target);
+const hashInput = $("#hash-input");
+const sendButton = $("#send-hash-btn");
+const remoteVideo = $("#remote-video");
 
-function goTrack(track) {
-  local_track = track.getTracks()[0];
-  local_video.srcObject = track;
+function connect() {
+  socket = new SockJS("/ws");
+  stompClient = Stomp.over(socket);
+  stompClient.connect({}, onConnected, onError);
 }
 
-navigator.mediaDevices
-  .getUserMedia({ video: true, audio: false })
-  .then(goTrack);
+function onConnected() {
+  stompClient.subscribe("/user/topic/rooms/join", ondirectMessage);
+}
 
-function connectSocket() {
-  socket = io("http://localhost:8080");
+function ondirectMessage(message) {
+  const { type, roomURL, sender } = JSON.parse(message.body);
 
-  socket.on("master", () => {
-    log("i am master");
-    isOffer = true;
+  isOffer = type === "MASTER";
+
+  myName = sender;
+  roomId = roomURL;
+
+  stompClient.subscribe(`/topic/rooms/${roomId}`, onRTC);
+
+  const message = {
+    type: "START",
+    sender,
+  };
+  stompClient.send(`/app/rooms/${roomId}`, {}, JSON.stringify(message));
+}
+
+function onRTC(message) {
+  const description = JSON.parse(message.body);
+  const { type, sender } = description;
+
+  if (sender && sender === myName) return;
+
+  switch (type) {
+    case "start":
+      {
+        startConnect();
+      }
+      break;
+    case "offer":
+      {
+        if (!isOffer) {
+          startConnect();
+        }
+        pc.setRemoteDescription(new RTCSessionDescription(description)).then(
+          () => doAnswer()
+        );
+      }
+      break;
+    case "answer":
+      {
+        if (isStarted)
+          pc.setRemoteDescription(new RTCSessionDescription(description));
+        break;
+      }
+      break;
+    case "candidate":
+      {
+        if (!isStarted) break;
+        const candidate = new RTCIceCandidate({
+          sdpMLineIndex: message.label,
+          candidate: message.candidate,
+        });
+
+        pc.addIceCandidate(candidate);
+        break;
+      }
+      break;
+  }
+}
+
+function startConnect() {
+  if (!isStarted) return;
+
+  createConnection();
+  pc.addStream(localStream);
+  isStarted = true;
+
+  if (isOffer) doCall();
+}
+
+function setLocalAndSendMessage(description) {
+  pc.setLocalDescription(description);
+  description.sender = myName;
+  emitSignerEvent(description);
+}
+
+function doCall() {
+  pc.createOffer(setLocalAndSendMessage, (e) => log(e + "\n call"));
+}
+
+function doAnswer() {
+  pc.createAnswer(setLocalAndSendMessage, (e) => log(e + "\n answer"));
+}
+
+function createConnection() {
+  pc = new RTCPeerConnection(pcConfig);
+  pc.onicecandidate = icecandidateHandler;
+  pc.onstream = remoteStream;
+}
+
+function icecandidateHandler(message) {
+  const { candidate } = JSON.parse(message.body);
+  if (!candidate) return;
+
+  const message = {
+    type: "CANDIDATE",
+    label: candidate.sdpMLineIndex,
+    id: candidate.sdpMid,
+    candidate: candidate.candidate,
+  };
+
+  emitSignerEvent(message);
+}
+
+function remoteStream(event) {
+  remoteVideo.srcObjec = event.stream;
+}
+
+function onSubmitHash() {
+  const hashqueue = hashInput.value.trim();
+  stompClient.send("/app/hash/" + hashqueue + "/join");
+}
+
+function onError(e) {
+  console.error(e);
+}
+
+function getNavigator() {
+  navigator.getUserMedia({ video: false, audio: true }, (stream) => {
+    localStream = stream;
   });
+}
 
-  socket.on("joined", () => {
-    console.log("joined");
-    isReady = true;
-  });
+getNavigator();
 
-  socket.on("message", (message) => {
+function emitSignerEvent(data) {
+  stompClient.send(`/app/rooms/${roomId}`, {}, JSON.stringify(data));
+}
+
+/*
+function startConnection() {
+  
+  if (!isStarted && typeof local_track !== "undefined" && isReady) {
+    createConnection();
+    pc.addTrack(local_track);
+    isStarted = true;
+
+    if (isOffer) {
+      doCall();
+    }
+  }
+}
+ */
+
+/*
+
+socket.on("message", (message) => {
     if (message === "start") {
       startConnection();
       return;
@@ -86,27 +212,10 @@ function connectSocket() {
         break;
       }
     }
-  });
-}
 
-function startConnection() {
-  console.log(isStarted, isReady, typeof local_track);
-  if (!isStarted && typeof local_track !== "undefined" && isReady) {
-    createConnection();
-    pc.addTrack(local_track);
-    isStarted = true;
+*/
 
-    if (isOffer) {
-      doCall();
-    }
-  }
-}
-
-function createConnection() {
-  pc = new RTCPeerConnection(pcConfig);
-  pc.onicecandidate = icecandidateHandler;
-  pc.ontrack = remoteTrackHandler;
-}
+/*
 
 function icecandidateHandler({ candidate }) {
   if (!candidate) return;
@@ -140,4 +249,4 @@ function setLocalAndSendMessage(description) {
   emitSignerEvent(description);
 }
 
-connectSocket();
+*/
